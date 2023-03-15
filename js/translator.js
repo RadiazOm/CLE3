78da09efb1994519f9c3b41f9763333430ba94e0
RadiazOm <jeffrey.otterloo@gmail.com>
2023-03-15 15:09:23 +0100

    funny ; go brrrrrr

--- js/translator.js
+++ js/translator.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', init)
+window.addEventListener('load', init);
 
 let form;
 let input;
@@ -9,11 +9,11 @@ let data;
 function init() {
     form = document.getElementById('input-field');
     input = document.getElementById('input');
-    output = document.getElementById('translated')
-    errorMessage = document.getElementById('error')
+    output = document.getElementById('translated');
+    errorMessage = document.getElementById('error');
 
-    form.addEventListener('submit', formClickHandler)
-    getJSONdata(`webservice/index.php?id=1`, configureData)
+    form.addEventListener('submit', formClickHandler);
+    getJSONdata(`webservice/index.php?id=1`, configureData);
 }
 
 function configureData(e) {
@@ -27,19 +27,17 @@ function formClickHandler(e) {
     for (const letter of array) {
         if (letter === " ") {
             let space = document.createElement('div')
-            space.classList.add('space')
-            output.appendChild(space)
+            space.classList.add('space');
+            output.appendChild(space);
         } else if (data.hasOwnProperty(letter)) {
-            let img = document.createElement('img')
-            img.src = `media/signs/${data[letter]}`
-            img.classList.add('sign')
-            output.appendChild(img)
+            let img = document.createElement('img');
+            img.src = `media/signs/${data[letter]}`;
+            img.classList.add('sign');
+            output.appendChild(img);
         } else {
-            output.innerHTML = 'Input has unknown character, please remove it'
+            output.innerHTML = 'Input has unknown character, please remove it';
         }
     }
-
-
 
     // output.innerHTML = input.value;
 }
