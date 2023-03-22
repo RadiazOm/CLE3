261c441c82c6572ccb7c09e534a32a691db60a26
fredsjr <Oli4schell@gmail.com>
2023-03-22 09:32:06 +0100

    localhost toegevoegd in de tekst bubbels

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,6 +1,29 @@
 window.addEventListener('load', init);
 
+let form;
+let inputField;
+let bubble;
+let bubbleContainer;
+
 function init(){
+
+form = document.getElementById('modal-form');
+inputField = document.getElementById('input-field');
+bubble = document.getElementById('bubble');
+bubbleContainer = document.getElementById('bubble-container')
+
+
+let bubbleItemString = localStorage.getItem('bubbleItems');
+    if (bubbleItemString !== null) {
+        let bubbleItems = JSON.parse(bubbleItemString)
+        console.log(bubbleItems)
+        for (let bubbleItem of bubbleItems){
+            addBubbleItem(bubbleItem)
+        }
+    }
+
+    form.addEventListener('submit', formSubmitHandler);
+
 //    modal voor de create form
 //    modal open
 document.getElementById("bubble-create").addEventListener("click", function (){
@@ -12,3 +35,28 @@ document.getElementById("bubble-create").addEventListener("click", function (){
         document.querySelector('.bg-modal').style.display = 'none';
     });
 });}
+
+function formSubmitHandler(e) {
+    e.preventDefault();
+    let inputValue;
+    console.log(inputField.value);
+    if (inputField.value !== '') {
+        inputValue = inputField.value;
+        let bubbleItems = JSON.parse(localStorage.getItem('bubbleItems')) || [];
+        bubbleItems.push(inputValue);
+        localStorage.setItem('bubbleItems', JSON.stringify(bubbleItems));
+        addBubbleItem(inputValue);
+        inputField.value = "";
+        console.log(inputValue);
+    }
+}
+
+function addBubbleItem(e){
+    let bubble = document.createElement('div');
+    bubble.classList.add('bubble', 'bubble-bottom-left');
+    let bubbleText = document.createElement('p')
+    bubbleText.innerHTML = e;
+    bubble.appendChild(bubbleText)
+    bubbleContainer.appendChild(bubble);
+    console.log('iets');
+}
