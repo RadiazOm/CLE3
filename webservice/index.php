ffc73fd89c000a2956b3be3389266d542422dc32
RadiazOm <jeffrey.otterloo@gmail.com>
2023-03-29 11:17:05 +0200

    changed the way the webservice works

--- webservice/index.php
+++ webservice/index.php
@@ -2,15 +2,33 @@
 //Require functions for actions
 require_once "includes/actions.php";
 
-switch ($_GET['id']) {
-    case 1:
-        $data = getSignLibrary();
-        break;
-    case 2:
+$data = 'API is not working';
+
+if ($_GET['webservice'] == 'translator') {
+    $data = getSignLibrary();
+}
+
+if ($_GET['webservice'] == 'info') {
+    if ($_GET['id'] === 'General'){
+        $data = 'no function yet';
+    } else if ($_GET['id'] === 'Popular'){
+        $data = ':/';
+    }
+    else if ($_GET['id'] === 'Communication'){
+        $data = ':/';
+    }
+    else{
         $data = getSportInfo();
-        break;
-    default:
-        $data = "error, cannot find data";
+
+    }
+}
+
+if ($_GET['webservice'] == 'drawing') {
+
+}
+
+if ($_GET['webservice'] == 'bubbles') {
+
 }
 
 //Set the header & output JSON so the client will know what to expect.
